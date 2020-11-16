c8f0c8bc27c8714994ee25a5822a4e50c37d9b57
James Dawson <jamesdawsonWD@gmail.com>
2020-11-16 13:13:36 +0000

    restructuring to mono repo

--- test/_helpers/planetManager.js
+++
@@ -1,9 +0,0 @@
-import { PlanetManagerC } from '../_utils/artifacts';
-export async function createPlanet(signedCall, from) {
-    const PlanetManager = await PlanetManagerC.deployed();
-    return await PlanetManager.createPlanet(signedCall, { from });
-}
-export async function getPlanetImplementation(from) {
-    const PlanetManager = await PlanetManagerC.deployed();
-    return await PlanetManager.planetImplementation({ from });
-}
